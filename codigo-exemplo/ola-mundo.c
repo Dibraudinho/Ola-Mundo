801622212366ec3496aee37576b47852aee1df73
ThiagoSantos21 <thiagolima-santos@hotmail.com>
2022-03-21 15:20:58 -0300

    Criação da função

--- codigo-exemplo/ola-mundo.c
+++ codigo-exemplo/ola-mundo.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+void classificarImc(float imc){
+
+    printf("IMC: %.2f\n",imc);
+
+    if(imc < 18.5)
+        printf("Magreza");
+    else if(imc < 24.9)
+        printf("Saudavel");
+    else if(imc < 29.9)
+        printf("Sobrepeso");
+    else if(imc < 34.9)
+        printf("Obesidade 1");
+    else if(imc < 39.9)
+        printf("Obesidade 2");
+    else
+        printf("Obesidade 3");
+}
+
 int main(){
-    printf("Hello, World!");
+   
+   float imc=20;
+
+   classificarImc(imc);
 }
