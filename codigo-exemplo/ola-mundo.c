47b65b8e727e67df38e7494a25f8c42021902835
ThiagoSantos21 <thiagolima-santos@hotmail.com>
2022-03-21 15:14:44 -0300

    Criação da função

--- codigo-exemplo/ola-mundo.c
+++ codigo-exemplo/ola-mundo.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int calcularImc(float peso, float altura){
+    return peso /(altura*altura);
+}
+
 int main(){
-    printf("Hello, World!");
+    float peso,altura,imc;
+
+    printf("Digite seu peso : ");
+    scanf("%f",&peso);
+    printf("Digite sua altura : ");
+    scanf("%f",&altura);
+
+    imc = calcularImc(peso,altura);
+
+    printf("IMC : %.2f",imc);
+
+
 }
